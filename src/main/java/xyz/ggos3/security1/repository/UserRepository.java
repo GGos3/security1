a0206d3ba812de8c2487132763d64e470fd293d8
GGos3 <janpll0421@gmail.com>
2023-05-02 22:13:04 +0900

    findByUsername 메서드 추가

--- src/main/java/xyz/ggos3/security1/repository/UserRepository.java
+++ src/main/java/xyz/ggos3/security1/repository/UserRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import xyz.ggos3.security1.model.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    public User findByUsername(String username);
 }
